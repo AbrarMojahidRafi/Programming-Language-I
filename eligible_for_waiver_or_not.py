97169e30f1e09d7440d42725e4585756a224dae6
AbrarMojahidRafi <rafi.cse.bracu@gmail.com>
2023-01-06 17:57:41 +0600

    added 4 files

---
+++ eligible_for_waiver_or_not.py
@@ -0,0 +1,45 @@
+"""Write a python program that takes the CGPA and no of credits completed by a student and prints whether the student is eligible for a waiver and of what percentage.
+
+To be eligible for a waiver, a student must have completed at least 30 credits and earned a CGPA greater or equal to 3.8. If not, please print "The students is not eligible for a waiver".
+
+CGPA-----------------------Waiver percentage
+3.80 - 3.89------------------25 percent
+3.90 - 3.94------------------50 percent
+3.95 - 3.99------------------75 percent
+4.00-------------------------100 percent
+
+Sample Input 1:
+3.93
+78
+
+Sample Output 1:
+The student is eligible for a waiver of 50 percent.
+
+================================================================================
+
+Sample Input 2:
+3.79
+24
+
+Sample Output 2:
+The students is not eligible for a waiver
+"""
+
+
+#todo
+
+cgpa=float(input("Enter your CGPA: "))
+noc=int(input("Enter your Credit: "))      #noc=no of credit
+
+if ( (3.80<= cgpa and cgpa <= 3.89) and noc>29):
+  print("The student is eligible for a waiver of 25 percent.")
+elif ( (3.90<=cgpa and cgpa<=3.94) and noc>29):
+  print("The student is eligible for a waiver of 50 percent.")
+elif ( (3.95<=cgpa and cgpa<=3.99) and noc>29):
+  print("The student is eligible for a waiver of 75 percent.")
+elif ( (4.00==cgpa) and noc>29.):
+  print("The student is eligible for a waiver of 100 percent.")
+else:
+  print("The students is not eligible for a waiver.")
+
+
---
+++ studentMarks_to_grades.py
@@ -0,0 +1,37 @@
+"""Write the Python code of a program that reads a student’s mark for a single subject, and prints out the corresponding grade for that mark. The mark ranges and corresponding grades are shown in the table below. You need to make sure that the mark is valid. For example, a student cannot receive -5 or 110 marks. So, the valid marks range is 0 to 100.
+
+hint(1): you can consider the number to be an integer
+
+hint(2): this problem can be solved in two ways: top-down (starts from A) and bottom-up (starts from F)
+
+Marks	        Grade
+90 or above	    A
+80-89	          B
+70-79	          C
+60-69	          D
+50-59	          E
+Below 50	      F
+
+"""
+
+
+#todo
+
+
+marks=int(input("Enter your marks: "))
+
+if ((marks<0) or (marks>100)):
+  print("This marks is not valid")
+elif (90<= marks <=100):
+  print("Your Grade is: A")
+elif (80<= marks <=89):
+  print("Your Grade is: B")
+elif (70<= marks <=79):
+  print("Your Grade is: C")
+elif (60<= marks <=69):
+  print("Your Grade is: D")
+elif (50<= marks <=59):
+  print("Your Grade is: E")
+else: 
+  print("Your Grade is: F")
+
