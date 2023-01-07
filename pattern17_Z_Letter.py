42ae6c7437ff8e340a9bcaf93244de81a27ea263
AbrarMojahidRafi <109167514+AbrarMojahidRafi@users.noreply.github.com>
2023-01-06 23:34:42 -0800

    Delete pattern17_Z_Letter.py

--- pattern17_Z_Letter.py
+++
@@ -1,9 +0,0 @@
-n=int(input("Enter the value of n: "))
-
-for row in range(n):
-  for column in range(n):
-    if ( row==0 or row==n-1 or ((row+column) == (n-1)) ) :
-      print("*" , end=" ")
-    else: 
-      print(" " , end=" ")
-  print()
